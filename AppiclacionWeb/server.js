e4fe1fd12a7ae759673d50f02328900aa79981c3
enmanuelmag <emmanu2006@hotmail.com>
2020-03-18 16:25:01 -0500

    Init pagina web

---
+++ AppiclacionWeb/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+  console.log('pong!')
+ return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080, () => {console.log('Server funcionando!')});
---
+++ AppiclacionWeb/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Container from '@material-ui/core/Container';
+import './App.css';
+
+import PasoAPaso from './components/PasoAPaso'
+
+function ElevationScroll(props) {
+	const { children, window } = props;
+	// Note that you normally won't need to set the window ref as useScrollTrigger
+	// will default to window.
+	// This is only being set here because the demo is in an iframe.
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0,
+		target: window ? window() : undefined
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0
+	});
+}
+
+ElevationScroll.propTypes = {
+	children: PropTypes.element.isRequired,
+	/**
+	 * Injected by the documentation to work in an iframe.
+	 * You won't need it on your project.
+	 */
+	window: PropTypes.func
+};
+
+function App(props) {
+	return (
+		<div className='App'>
+			<body className='App-body'>
+				<React.Fragment>
+					<CssBaseline />
+					<ElevationScroll {...props}>
+						<AppBar>
+							<Toolbar>
+								<Typography variant='h6'>Mi Horario Web</Typography>
+							</Toolbar>
+						</AppBar>
+					</ElevationScroll>
+					<Toolbar />
+					<Container>
+						<PasoAPaso/>
+					</Container>
+				</React.Fragment>
+			</body>
+		</div>
+	);
+}
+
+export default App;
