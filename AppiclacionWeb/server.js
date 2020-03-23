63d44a8a64f5552e09ca5fd11f26405da9566bd8
jcoboss <josuealexandercs15@gmail.com>
2020-03-23 00:36:25 -0500

    consultas materias y malla (parcial)

--- AppiclacionWeb/server.js
+++ AppiclacionWeb/server.js
@@ -1,12 +1,31 @@
+const codigoNombre = require("./server_resources/codigoNombre");
+const mallas = require("./server_resources/mallas") ;
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.get('/ping', function (req, res) {
   console.log('pong!')
  return res.send('pong');
+});
+
+//Esta función retorna un objeto de la forma
+//{<codigo>:<nombre-mateira>, ...}
+app.get('/codigo-materias', function (req, res) {
+  return res.send(codigoNombre['codigoNombre']);
+});
+
+//Esta función retorna un objeto de la forma
+//{<facultad>: {<carrera>: [codigo1,codigo 2...]...}...}
+app.get('/mallas', function (req, res) {
+  return res.send(mallas["mallas"]);
 });
 
 app.get('/', function (req, res) {
