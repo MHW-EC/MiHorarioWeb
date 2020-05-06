986e3f55ca479e7466d96716b26b2a105d10e293
enmanuel-mag <emmanu2006@hotmail.com>
2020-05-06 01:35:44 -0500

    Nueva db

--- src/server/database/db.js
+++ src/server/database/db.js
@@ -1,4 +1,5 @@
-const URI = process.env.MONGODB_URI || 'mongodb://localhost/horarioweb';
+const URI =
+	'mongodb+srv://cardor:Enma19624@heroku-28zqn5s2-htt13.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
 	db: URI,
