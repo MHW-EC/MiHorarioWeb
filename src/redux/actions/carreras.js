6d62133cc8e998d57594d721f03533280e3c86eb
Josue Cobos <josuealexandercs15@gmail.com>
2021-04-17 16:56:32 -0500

    [SearchCarrera] Add loading state to component

--- src/redux/actions/carreras.js
+++ src/redux/actions/carreras.js
@@ -1,15 +1,7 @@
-
-export const GET_FETCH_CARRERAS = 'GET_FETCH_CARRERAS'
-
 export const GET_CARRERAS_START = 'GET_CARRERAS_START'
 export const GET_CARRERAS_ERROR = 'GET_CARRERAS_ERROR'
 export const GET_CARRERAS_COMPLETE = 'GET_CARRERAS_COMPLETE'
 
-
-
-
 export const getCarreras = () => ({
     type: GET_CARRERAS_START
 })
-
-
