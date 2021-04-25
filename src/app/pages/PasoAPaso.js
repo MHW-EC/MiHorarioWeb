3f74f25b43161b228cdd04404e7b38f4bf79591b
Josue Cobos <josuealexandercs15@gmail.com>
2021-04-25 14:04:17 -0500

    Fix margin bug

--- src/app/pages/PasoAPaso.js
+++ src/app/pages/PasoAPaso.js
@@ -280,7 +280,7 @@ export default function PasoAPaso() {
           </Step>
         ))}
       </Stepper>
-      <Grid container spacing={3} style={{margin: '0px'}}>
+      <Grid container spacing={3}>
         <Grid item xs={12}>
           <Typography 
             className={classes.instructions} 
