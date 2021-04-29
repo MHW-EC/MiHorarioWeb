b5a7897a85812e608802a9eaa46523a1f592b749
Enmanuel Magallanes Pinarogte <fmagalla@fiec.espol.edu.ec>
2021-04-28 21:43:48 -0500

    Add icon to image

--- src/app/components/paso4/horario-visor.js
+++ src/app/components/paso4/horario-visor.js
@@ -85,7 +85,7 @@ export default function NavTabs(props) {
 	const [horario, setHorario] = React.useState(props.horario);
 	//const horario = React.useState(props.horario);
 	const [panel, setPanel] = React.useState(0);
-	const oculto = true;
+	const oculto = false;
 	const dispatch = useDispatch();
 	const enqueueSnackbar = (...args) => dispatch(enqueueSnackbarAction(...args));
 	const closeSnackbar = (...args) => dispatch(closeSnackbarAction(...args));
